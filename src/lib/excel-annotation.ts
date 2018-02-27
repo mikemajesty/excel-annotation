4e7215662c0ad35621c04cf8abedd17f315c7ac1
mikemajesty <mikemajesty2011@gmail.com>
2018-02-27 00:54:56 -0300

    add styles

--- src/lib/excel-annotation.ts
+++ src/lib/excel-annotation.ts
@@ -11,28 +11,14 @@ export async function createExcel(res): Promise<any> {
     workbook.lastPrinted = new Date(2016, 9, 27);
     var sheet = workbook.addWorksheet('My Sheet', {properties:{tabColor:{argb:'FFC0000'}}});
     sheet.columns = [
-        { header: 'Id', key: 'id', width: 10 },
-        { header: 'Name', key: 'name', width: 32 },
-        { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
+        { header: 'Estado', key: 'state', width: 10, style: { font: { name: 'Arial Black' } }  },
+        { header: 'Cidade', key: 'city', width: 32, style: { font: { name: 'Arial Black' } } },
+        { header: 'País', key: 'country', width: 10, style: { font: { name: 'Arial Black' } }  }
     ];
     
-    // Access an individual columns by key, letter and 1-based column number
-    var idCol = sheet.getColumn('id');
-    var nameCol = sheet.getColumn('B');
-    var dobCol = sheet.getColumn(3);
-    //https://github.com/guyonroche/exceljs#set-workbook-properties
-    // set column properties
-    
-    // Note: will overwrite cell value C1
-    dobCol.header = 'Date of Birth';
-    
-    // Note: this will overwrite cell values C1:C2
-    dobCol.header = ['Date of Birth', 'A.K.A. D.O.B.'];
-    
-    // from this point on, this column will be indexed by 'dob' and not 'DOB'
-    dobCol.key = 'dob';
-    
-    dobCol.width = 15;
+    sheet.addRow({state: "SP", city: 'Mike Lima', country: "Brasil"});
+    sheet.addRow({state: "PR", city: 'Jane Barbosa', country: "Brasil"});
+
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
     return await workbook.xlsx.write(res)
